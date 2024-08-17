0d6fc41fec9d1f51dfec7bc48c95d597f2c85d8f
ermi4sol <ermi4sol@gmail.com>
2024-08-17 11:50:08 +0300

    task 0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
+"""
+Module to fetch the number of subscribers for a given subreddit
+using the Reddit API.
+"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+
+    Parameters:
+    subreddit (str): The name of the subreddit.
+
+    Returns:
+    int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the request was successful
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data['data']['subscribers']
     else:
         return 0
